73188a89d8c15d64a8c98b71515420073456d411
YangBiao <biaoy@outlook.jp>
2020-12-24 17:14:30 +0900

    20/12/24 update

--- 2020/1224/1224.py
+++ 2020/1224/1224.py
@@ -283,33 +283,3 @@ def question_31():
 
 
 question_31()
-
-
-# 文本颜色设置。
-# class question_32:
-#     HEADER = '\033[95m'
-#     OKBLUE = '\033[94m'
-#     OKGREEN = '\033[92m'
-#     WARNING = '\033[93m'
-#     FAIL = '\033[91m'
-#     ENDC = '\033[0m'
-#     BOLD = '\033[1m'
-#     UNDERLINE = '\033[4m'
-#
-#
-# print('Question 32: ', question_32.WARNING + "警告的颜色字体?")
-
-
-# 求100之内的素数。
-def question_33():
-    result = []
-    for n in range(2, 101):
-        for j in range(2, n):
-            if n % j == 0:
-                break
-        else:
-            result.append(n)
-    print('Question 33: ', result)
-
-
-question_33()
